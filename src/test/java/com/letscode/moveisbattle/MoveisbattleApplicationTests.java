c17a477ae985c5a760e3cb54b05b708efe8727bc
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2022-02-19 00:01:35 -0300

    Initial commit

---
+++ src/test/java/com/letscode/moveisbattle/MoveisbattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.moveisbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveisbattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
